cc38d7072625e9e4cfc3c467150a1d321d97175e
preman ayam <sweetestname013@gmail.com>
2018-07-17 21:10:55 +0700

    skrip pertama

---
+++ public/sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+	createCanvas(500, 500);
+	background(51);
+	
+	socket = io.connect('http://localhost:3000');
+	socket.on('mouse',newDrawing);
+}
+
+function newDrawing(data){
+	noStroke();
+	fill(255, 0, 100);
+	ellipse(data.x,data.y,10,10);
+}
+
+function mouseDragged(){
+	console.log(mouseX + ' + ' + mouseY);
+	noStroke();
+	fill(255);
+	
+	ellipse(mouseX,mouseY,10,10);
+	
+	data = {
+		x: mouseX,
+		y: mouseY
+	}
+	
+	socket.emit('mouse', data);
+	
+}
+
+function draw() {
+	
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
+console.log("server running");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.on('connection', newConnection);
+
+function newConnection(socket){
+	console.log('new Connection : ' + socket.id);
+	
+	socket.on("mouse",mouseMsg);
+	function mouseMsg(data){
+		socket.broadcast.emit('mouse',data);
+	}	
+}
+
