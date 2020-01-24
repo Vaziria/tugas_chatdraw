75c1ba896eb38d8724872e40fd0ec9d4406f43d1
preman ayam <sweetestname013@gmail.com>
2020-01-24 07:57:11 +0700

    add mongo db

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path');
 const mongo = require('mongodb').MongoClient;
 const BodyParser = require("body-parser");
 
-var database, coluser;
+var database, coluser, colmessage;
 
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
@@ -26,6 +26,7 @@ var server = app.listen(3000, () => {
 
 	        database = db.db('roses');
 	        coluser = database.collection("user");
+	        colmessage = database.collection("message");
 
 	});
 
@@ -53,12 +54,32 @@ io.on('connect', newConnection);
 
 function newConnection(socket){
 	var name = 'kampretcode';
+
+	coluser.updateOne({'_id': name}, {$set: { online: true}}, upsert = true)
 	console.log(name, 'connect')
 
 	socket.broadcast.emit('newuser', name);
 
+	// getMessage
+	colmessage.find({}).limit(50).toArray((error, result) => {
+        if(error) {
+            return false;
+        }
+        socket.emit('history_msg', result);
+    });
+
+    // get user
+    coluser.find({}).limit(50).sort({online: 1}).toArray((error, result) => {
+        if(error) {
+            return false;
+        }
+        socket.emit('history_msg', result);
+    });
+
+
 	socket.on("disconnect", function(){
 		console.log(name, 'disconnect')
+		coluser.updateOne({'_id': name}, {$set: { online: false}}, upsert = true)
 	})
 	
 	socket.on("mouse", mouseMsg);
